14604ef57beb6e945e5eb3efc3bc76b61db1b1d0
Marsha <marshakshearin@gmail.com>
2020-01-28 07:12:03 -0500

    continue

--- homework.js
+++ homework.js
@@ -15,17 +15,23 @@ greeting();
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+let number = 3
 function isOdd(number) {
-    if (number % 2 === 1) { console.log("true") }
-    else { console.log("false") }
+    if (number % 2 === 1 || number % 2 === -1) { return "true" }
+    else { return "false" }
 }
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(number) {
+    if (number % 2 === 0) { return "true" }
+    else { return "false" }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
